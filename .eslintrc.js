87510a458d002d3a78648bb5c95d776682dc5a64
vpogodaev <valek.pogodaev@yandex.ru>
2022-05-02 14:40:18 +0300

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,10 +17,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
@@ -29,9 +28,10 @@ module.exports = {
     'react/function-component-definition': 'off',
     'no-param-reassign': 'off',
     'no-return-await': 'off',
-    'no-unused-vars': 'warn',
     'no-empty-pattern': 'warn',
     'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     // TODO: 'react/jsx-closing-bracket-location':
   },
 };
